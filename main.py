251dd6eb29961ae9374d38f48f684c49c231f2a8
KAGunter <112012002+KAGunter@users.noreply.github.com>
2025-01-10 18:45:43 -0600

    Update main.py
    testing something

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
 while True:
 	for i in range(get_world_size()):
-		harvest()
+		if(can_harvest()):
+			harvest()
 		if(i < (get_world_size()-1)):
 			move(North)
-		
+	
+	move(North)
 	move(East)
 	
-	for i in range(3):
-		harvest()
+	for i in range(get_world_size()):
+		if(can_harvest()):
+			harvest()
+			plant(Entities.Bush)
 		if(i < (get_world_size()-1)):
-			move(South)
-		
+			move(North)
+	
+	move(North)
+	move(East)
+	
+	for i in range(get_world_size()):
+		if(can_harvest()):
+			harvest()
+			plant(Entities.Bush)
+		if(i < (get_world_size()-1)):
+			move(North)
+	
+	move(North)
 	move(East)
 	
 	for i in range(get_world_size()):
@@ -18,30 +33,6 @@ while True:
 			harvest()
 		if(i < (get_world_size()-1)):
 			move(North)
-		
-	move(East)
 	
-	for i in range(3):
-		do_a_flip()
-		harvest()
-		plant(Entities.Bush)
-		if(i < (get_world_size()-1)):
-			move(South)
-			
-	move(East)
-		
-	for i in range(get_world_size()):
-		do_a_flip()
-		harvest()
-		plant(Entities.Bush)
-		if(i < (get_world_size()-1)):
-			move(North)
-		
-	move(East)
-	for i in range(3):
-		do_a_flip()
-		plant(Entities.Bush)
-		if(i < (get_world_size()-1)):
-			move(South)
-		
+	move(North)
 	move(East)
